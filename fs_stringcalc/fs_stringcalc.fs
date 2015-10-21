d0b20ea6c5e587068c91da917c8c9deff0e042d3
Vince Lee <vslee888@gmail.com>
2015-10-21 19:28:27 +0100

    single number gets returned as number

--- fs_stringcalc/fs_stringcalc.fs
+++ fs_stringcalc/fs_stringcalc.fs
@@ -1,11 +1,18 @@
 ﻿module fs_stringcalculator
 
 open NUnit.Framework
+open System
 
 let stringCalc(input) =
-    0
+    if input = "" then 0
+    else Int32.Parse input
 
 [<Test>]
 let ``Empty string equals 0``() = 
     let result = stringCalc ""
     Assert.That(result, Is.EqualTo 0)
+
+[<Test>]
+let ``Single number in string equals same number``() =
+    let result = stringCalc "8"
+    Assert.That(result, Is.EqualTo 8)
