cac9c349450671575350cd746c791ff7e9e77203
Vince Lee <vslee888@gmail.com>
2015-10-21 20:01:49 +0100

    input can define delimiter

--- fs_stringcalc/fs_stringcalc.fs
+++ fs_stringcalc/fs_stringcalc.fs
@@ -3,11 +3,22 @@
 open NUnit.Framework
 open System
 
+let getDelimiter(input:string) =
+    match input.StartsWith "//" with 
+    | true -> input.[2]
+    | false -> ','
+
+let getNumbers(input:string) =
+    match input.StartsWith "//" with 
+    | true -> input.Substring(input.IndexOf("\n") + 1)
+    | false -> input
+
 let stringCalc(input:string) =
-    let delimiter = ','
-    match input.Length with
+    let delimiter = getDelimiter input
+    let values = getNumbers input
+    match values.Length with
     | 0 ->  0
-    | _ ->  input.Replace('\n', delimiter).Split delimiter
+    | _ ->  values.Replace('\n', delimiter).Split delimiter
             |> Seq.map Int32.Parse
             |> Seq.sum
 
@@ -31,7 +42,7 @@ let ``New line can act as delimiter``() =
     let result = stringCalc "1\n2,3"
     Assert.That(result, Is.EqualTo 6)
 
-//[<Test>]
-//let ``Input can define delimiter``() =
-//    let result = stringCalc "//;\n1,2,3"
-//    Assert.That(result, Is.EqualTo 6)
+[<Test>]
+let ``Input can define delimiter``() =
+    let result = stringCalc "//;\n1;2;3"
+    Assert.That(result, Is.EqualTo 6)
