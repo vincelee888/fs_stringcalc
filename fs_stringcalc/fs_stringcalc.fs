6082fa6b313302a60cf2e9aa3a527d1abfa499ae
Vince Lee <vslee888@gmail.com>
2015-10-21 19:26:16 +0100

    Empty string equals 0

--- fs_stringcalc/fs_stringcalc.fs
+++ fs_stringcalc/fs_stringcalc.fs
@@ -1,4 +1,11 @@
-﻿module fs_stringcalc
+﻿module fs_stringcalculator
 
 open NUnit.Framework
 
+let stringCalc(input) =
+    0
+
+[<Test>]
+let ``Empty string equals 0``() = 
+    let result = stringCalc ""
+    Assert.That(result, Is.EqualTo 0)
